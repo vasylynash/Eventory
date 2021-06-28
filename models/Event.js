7c579f5f6c2c7416362a5fb2c294d1353ae8928f
Vasylyna Shevadutska <vaskash@gmail.com>
2021-06-28 12:00:02 -0700

    Added models; fixed seed data (#5)
    * Added models and connection.js
    
    * Added schema and fixed typo in folder name
    
    * Added participants model; added participants.json; fixed seed.js
    
    * Updated Event model

---
+++ models/Event.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User');
+const Category = require('./Category');
+
+class Event extends Model {}
+
+Event.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dateTime: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true,
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Category,
+                key: 'id',
+            },
+        },
+        owner_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: User,
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'event',
+    }
+);
+
+module.exports = Event;
