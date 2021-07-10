23c59ca343d25ef47165208950310f091ea66067
Vasylyna Shevadutska <vaskash@gmail.com>
2021-07-10 08:34:42 -1000

    Added username to cards (#18)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
-const { Event } = require('../models');
+const { Event, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async function (req, res) {
-  const eventData = await Event.findAll();
+  const eventData = await Event.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  });
 
   const events = eventData.map((el) => el.get({ plain: true })); 
+  console.log(events);
   
   res.render('all',{
     events
