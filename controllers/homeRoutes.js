e2920a934a8d63f95ae2639560d0e74c3f20ebba
Vasylyna Shevadutska <vaskash@gmail.com>
2021-07-12 11:04:17 -1000

    Added joined events view (#24)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { Event, User } = require('../models');
+const { Event, User, Participants } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async function (req, res) {
+  try {
   const eventData = await Event.findAll({
     include: [
       {
@@ -17,20 +18,46 @@ router.get('/', async function (req, res) {
   res.render('all',{
     events
   });
+  }
+  catch (e) {
+    res.json(e);
+  }
 });
 
 router.get('/dashboard', withAuth, async function (req, res) {
+  try {
   const eventData = await Event.findAll({
     where: {
       owner_id: req.user.id,
     },
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
   });
 
   const events = eventData.map((el) => el.get({ plain: true }));
-  
+
+  const joinedEventsData = await req.user.getJoined({
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  });
+  const joinedEvents = joinedEventsData.map((el) => el.get({ plain: true }));
+
   res.render('userCardsAll',{
-    events
-  })
+    events,
+    joinedEvents
+  });
+  }
+  catch(e) {
+    res.json(e);
+  }
 });
 
 module.exports = router;
