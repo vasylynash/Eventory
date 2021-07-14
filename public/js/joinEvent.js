23035400c2957a59d50769ffa3d3d00d18905812
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-07-13 18:28:30 -0700

    removed console.log

--- public/js/joinEvent.js
+++ public/js/joinEvent.js
@@ -4,7 +4,6 @@ const joinEvent = async (event) => {
     event.preventDefault();
     const currentuserid = event.target.getAttribute('data-currentuserid');
     const eventid = event.target.getAttribute('data-eventid');
-    console.log(currentuserid,eventid)
       const response = await fetch(`/api/events/join`, {
         method: 'POST',
         body: JSON.stringify({
